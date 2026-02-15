79e616fe0ef97d9b82509d390035dc759d13d61a
Nadira Aliya Nashwa <nadiraalia4@gmail.com>
2026-02-15 16:38:36 +0700

    chore: initialize vue with dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { Toaster } from 'vue-sonner'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Toaster position="top-center" richColors />
+  <RouterView />
 </template>
-
-<style scoped></style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-screen">
+    <div class="flex flex-col items-center gap-6">
+      <h1 class="text-6xl font-bold text-center text-blue-400">
+        Inventra DD <span class="text-green-700">Vue</span>
+      </h1>
+    </div>
+  </div>
+</template>
